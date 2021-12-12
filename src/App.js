f6b5cd0f93bbc422dd96080bdac5784780b1bf7b
Naman-Saxena1 <namansaxena.official@gmail.com>
2021-12-12 18:23:15 +0530

    deploy to heroku

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {React, useState} from 'react';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+
+  const [loginData,setLoginData] = useState(
+    localStorage.getItem('loginData')
+    ? JSON.parse(localStorage.getItem('loginData'))
+    : null
+  )
+
+  const handleLogin = async (googleData) => {
+    console.log(googleData)
+
+    const res = await fetch('/api/google-login',{
+      method : 'POST',
+      body : JSON.stringify({token : googleData.tokenId}),
+      headers : {'Content-Type' : 'application/json'}
+    })
+
+    const data = await res.json();
+    setLoginData(data)
+    localStorage.setItem('loginData',JSON.stringify(data))
+  }
+
+  const handleFailure = (result) => {
+    alert(result)
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('loginData');
+    setLoginData(null)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        <div>
+          {
+            loginData 
+            ? (
+              <div>
+                <h3>You logged in as {loginData.email}</h3>
+                <button style={{minWidth:'10rem',minHeight:'3rem'}}onClick={handleLogout}>Logout</button>
+              </div>
+            )
+            :
+            <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Login with Google"
+            onSuccess={handleLogin}
+            onFailure={handleFailure}
+            cookiePolicy={'single_host_origin'}>
+            </GoogleLogin>
+          }
+        </div>
       </header>
     </div>
   );
